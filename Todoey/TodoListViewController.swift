f42877c2573a59ed76c7e83b67a4def579802cf7
Patryk Uran <trykpa@gmail.com>
2018-07-31 19:59:12 +0200

    add saving user data and changes in app with userDefault object

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Like Sth", "Scream for no reason", "Lick @#$"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //MARK - Tableview Datasource Methods
@@ -58,6 +64,9 @@ class TodoListViewController: UITableViewController {
             //what will happen after Add Item Button pressed
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         
